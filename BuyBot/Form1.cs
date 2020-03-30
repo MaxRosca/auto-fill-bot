089b30a8acf23ae583115679fbc0bd88e371486e
Rosca Maxim <roscamaxim2002@gmail.com>
2020-03-30 17:07:28 +0300

    first commit

---
+++ BuyBot/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace BuyBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            IWebDriver driver = new ChromeDriver(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName);
+
+            driver.Navigate().GoToUrl(@"https://www.supremenewyork.com/shop/hats/uu15aw2pc/zbl6dtn1y");
+
+        }
+
+    }
+}
