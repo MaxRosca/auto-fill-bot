dfc0ae76a5e83744637159a934566b4ee6f3bb45
Rosca Maxim <roscamaxim2002@gmail.com>
2020-03-30 17:33:04 +0300

    a

--- BuyBot/Form1.cs
+++ BuyBot/Form1.cs
@@ -22,7 +22,10 @@ namespace BuyBot
 
             IWebDriver driver = new ChromeDriver(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName);
 
-            driver.Navigate().GoToUrl(@"https://www.supremenewyork.com/shop/hats/uu15aw2pc/zbl6dtn1y");
+            driver.Navigate().GoToUrl(@"https://www.g2g.com/sell/manage?service=18&game=22666");
+
+            driver.FindElement(By.XPath("//*[@id='c2c_4753988']/td[2]/div/div[2]/span[2]/a")).Click();
+
 
         }
 
